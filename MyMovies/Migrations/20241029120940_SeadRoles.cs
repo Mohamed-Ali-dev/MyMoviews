bf545d14c3c47d58b317ea164eac76f915457713
mali <moohamedali571@gmail.com>
2024-10-29 15:15:46 +0300

    Sead Roles

---
+++ MyMovies/Migrations/20241029120940_SeadRoles.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MyMovies.Utility;
+
+#nullable disable
+
+namespace MyMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeadRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+              table: "AspNetRoles",
+              columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+              values: new object[] { Guid.NewGuid().ToString(), SD.Role_User, SD.Role_User.ToUpper(), Guid.NewGuid().ToString() }
+              );
+
+            migrationBuilder.InsertData(
+           table: "AspNetRoles",
+           columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+           values: new object[] { Guid.NewGuid().ToString(), SD.Role_Admin, SD.Role_Admin.ToUpper(), Guid.NewGuid().ToString() }
+           );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [AspNetRoles]");
+
+        }
+    }
+}
--- MyMovies/obj/Debug/net8.0/MyMovies.AssemblyInfo.cs
+++ MyMovies/obj/Debug/net8.0/MyMovies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06177ca4df24d035cefbd47f516e6932d0854b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f27c7d70ff272a6872974987144e06681fe5497")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
